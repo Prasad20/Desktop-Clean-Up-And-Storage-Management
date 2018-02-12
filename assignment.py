26a66574d361f0fb6a9406a74cfe85a14fa5c775
Prasad20 <prasadparshallu20@gmail.com>
2018-02-13 01:01:24 +0530

    Update assignment.py

--- assignment.py
+++ assignment.py
@@ -1,87 +1,82 @@
 import os
 import sys
+import platform
 
-a = []
+a = []                                                          # stores the path and size of file in 2d matrix format
 
-seperator = ""
+seperator = ""                                                  # As seperator in windows and other os are different
 
-def function(path):
-    List = []
-    for dirpath, subdirs, files in os.walk(path):
+def search_in_directory(path):
+    pList = []                                                   #pList will store the path of file
+    for dirpath, subdirs, files in os.walk(path):               # for loop to search files in sub-folders
         for x in files:
-            List.append(os.path.join(dirpath, x))
+            pList.append(os.path.join(dirpath, x))
 
-    nlist = []
-    for file in List:
-        nlist.append(os.stat(file).st_size)
+    sList = []                                                  #slist[] will store the size of file
+    for file in pList:                                           # for loop to store size of files in List "pList"
+        sList.append(os.stat(file).st_size)
 
-    for i in range(len(List)):
-        a.append([List[i], nlist[i]])
+    for i in range(len(pList)):                                  # for loop to append path and size of file in list "a"
+        a.append([pList[i], sList[i]])
 
-    a.sort(key=lambda x: x[1])
+    a.sort(key=lambda x: x[1])                                   # sort the List "a" w.r.t size
 
-
-def func(directory):
-    List = []
-    for file in os.listdir(directory):
+def files_in_directory(directory):
+    pList = []                                                   # pList will store the path of file
+    for file in os.listdir(directory):                          # for loop to search files in directory
         filename = os.fsdecode(file)
         if not (filename.endswith(".exe") or filename.endswith(".lnk") or os.path.isdir(directory + "/" + file)):
-            List.append(os.path.join(directory, filename))
+            pList.append(os.path.join(directory, filename))
 
-    nlist = []
-    for file in List:
-        nlist.append(os.stat(file).st_size)
+    slist = []                                                   #slist[] will store the size of file
+    for file in pList:                                            # for loop to store size of files in List "pList"
+        slist.append(os.stat(file).st_size)
 
-    for i in range(len(List)):
-        a.append([List[i], nlist[i]])
-def setSep(w):
-    global seperator
-    if w == '1':
-        seperator = '\\'
-    else:
-        seperator = '/'
-
-w = input("Enter 1 for windows and 2 for Linux : ")
-
-setSep(w)
+    for i in range(len(pList)):                                    # for loop to append path and size of file in list "a"
+        a.append([pList[i], slist[i]])
 
 
-s = input("Enter number of Directories you want to look into: ")
+# Program starts from here
 
-l = []
-for i in range(int(s)):
+if (platform.system() == "Windows"):                              # As for windows directories has "\" as seperator and for linux it is "/" this is done by the following if
+    seperator = '\\'
+else:
+    seperator = '/'
+
+s = input("Enter number of Directories you want to search into: ")                        # s is a dummy variable just to store number of directories you want to search
+
+l = []                                                                  # l is a list storing the directories you want to search into
+
+for i in range(int(s)):                                                 # append the directories you want to search into
     k = input("Enter the Directory:")
     l.append(k)
 
-for i in range(int(s)):
-    function(l[i])
+for i in range(int(s)):                                                 # for loop will call function to search in directory for all directories you want entered before
+    search_in_directory(l[i])                                           # this function will store all the files path and size in the given directory and in its sub-folders in list a
 
-for x in (range(10) or (range(len(a)))):
+for x in (range(10) or (range(len(a)))):                                # Print 10 largest files in the directory or if files are less than 10 then print them all with their size in MB
     print('[%s]'"MB "'[%s]' % (a[-1 - x][1] / 1000000, a[-1 - x][0]))
 
-a.clear()
+a.clear()                                                               # Remove all the directories that you appended to search 10 largest files
 
-p = input("Enter your desktop directory ")
+path = input("Enter your desktop directory or any other directory in which you want to sort the files :")    # path will store the directory in which you want to move the files in a folder
 
-s = input("Enter the directory where you want to store desktop files :")
+newpath = input("Enter the directory where you want to move the files :")    # newpath will be store the directory where the files are to be moved
 
-func(p)
+files_in_directory(path)                                               # this function store all the_files_path and size in the_directory in list "a"
 
-newpath = s + seperator + "desktop"
+newpath+= seperator + "desktop"                                     # the newpath add folder name called ("desktop") to its path
 
-if not os.path.exists(newpath):
+if not os.path.exists(newpath):                                     # if desktop folder doesnt exists then desktop directory will be created
     os.makedirs(newpath)
 
-for x in range(len(a)):
-    old = a[-1 - x][0]
-    temp = old.split(seperator)
+for x in range(len(a)):                                              #this for loop will search all the files appended in list "a"
+    old = a[-1 - x][0]                                               # old will store the path of the file
+    temp = old.split(seperator)                                      # temp will store the file name with the extension
     temp = (temp[len(temp) - 1])
 
-    filen = old
-    filen += seperator + temp
-
-    filen = temp.split(".")
-    foldn = (filen[len(filen) - 1])
+    filen = temp.split(".")                                          # filen will store the file name with extension
+    foldn = (filen[len(filen) - 1])                                  # foldn will store the extension of the file
 
     if (foldn == "mp4"):
         foldn = ""
@@ -96,16 +91,17 @@ for x in range(len(a)):
         foldn = ""
         foldn = "WordDocuments"
 
-    newpath1 = newpath
-    newpath1 += seperator + foldn
+    newpath1 = newpath + seperator + foldn                            #newpath1 will create the path of sub-folder of desktop folder
 
-    if not os.path.exists(newpath1):
+    if not os.path.exists(newpath1):                                  #if sub-folder already exists then no folder is created
         os.makedirs(newpath1)
 
     tfilen = ""
-    tfilen = str(newpath1) + seperator + str(temp)
+    tfilen = str(newpath1) + seperator + str(temp)                  #tfilen will create the path of file to be moved in the sub folder
 
-    if not os.path.exists(tfilen):
+    if not os.path.exists(tfilen):                                 #if the file exists already in the subfolder then file is not copied again in the sub-folder
         os.rename(old, tfilen)
     else:
-        os.remove(old)
+        os.remove(old)                                             #the file in the driectory is removed
+
+print("Your files are successfully moved in the folder called desktop")
